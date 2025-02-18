7209066897a4cbef15d53a0501fd0616159ac70b
Okpalaeze patrick Echezona <129857369+Echez-cpu@users.noreply.github.com>
2025-02-18 13:56:50 +0100

    Update and rename Command.hpp to Whatever.hpp

---
+++ ex00/includes/Whatever.hpp
@@ -0,0 +1,18 @@
+#ifndef   WHATEVER_HPP
+#define   WHATEVER_HPP
+
+#include <iostream>
+#include <string>
+
+typedef std::string str;
+
+template<typename T>
+void	swap(T &a, T &b);
+
+template<typename T>
+T	min(T a, T b);
+
+template<typename T>
+T	max(T a, T b);
+
+#endif
