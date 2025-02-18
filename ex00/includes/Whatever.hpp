002a41fe0fb9a40beb5813f551cfc8f1cdcece8f
Okpalaeze patrick Echezona <129857369+Echez-cpu@users.noreply.github.com>
2025-02-18 14:38:12 +0100

    Update Whatever.hpp

--- ex00/includes/Whatever.hpp
+++ ex00/includes/Whatever.hpp
@@ -10,9 +10,9 @@ template<typename T>
 void	swap(T &a, T &b);
 
 template<typename T>
-T	min(const T a, const T b);
+T	min(const T &a, const T &b);
 
 template<typename T>
-T	max(const T a, const T b);
+T	max(const T &a, const T &b);
 
 #endif
