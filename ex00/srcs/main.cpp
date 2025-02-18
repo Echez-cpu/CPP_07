3e310519fae19f73e7376f4e3ea570603e2acbdb
Okpalaeze patrick Echezona <129857369+Echez-cpu@users.noreply.github.com>
2025-02-18 14:01:08 +0100

    Create main.cpp

---
+++ ex00/srcs/main.cpp
@@ -0,0 +1,28 @@
+#ifndef ../includes/WHATEVER_HPP
+#define ../includes/WHATEVER_HPP
+
+#include <iostream>
+
+template <typename T>
+void swap(T &a, T &b) {
+    T temp = a;
+    a = b;
+    b = temp;
+}
+
+template <typename T>
+T min(const T &a, const T &b) {
+    if (a < b)
+        return a;
+    else
+        return b;
+}
+
+template <typename T>
+T max(const T &a, const T &b) {
+    if (a > b)
+        return a;
+    else
+        return b;
+}
+
